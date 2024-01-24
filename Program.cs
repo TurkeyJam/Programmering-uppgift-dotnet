9645d3dbaa367aee5cbafada8b8853b2895cc8d6
emilmajava <h22emma@axevalla.fhsk.se>
2024-01-24 11:17:41 +0100

    Fullfört Get started del 1

--- Program.cs
+++ Program.cs
@@ -8,7 +8,16 @@ Game.Instance.Dispose();
 
 static void Startup(object? sender, GameHost host)
 {
-    Game.Instance.StartingConsole!.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole!.Font);
     Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
     Game.Instance.StartingConsole.Print(4, 4, "Hello from SadConsole");
+    Game.Instance.StartingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateBorder(new ColoredGlyph(Color.Violet, Color.Black, 176)));
+    Game.Instance.StartingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateStyledBox(ICellSurface.ConnectedLineThin, new ColoredGlyph(Color.Violet, Color.Black)));
+    Game.Instance.StartingConsole.DrawCircle(new Rectangle(5, 8, 16, 8), ShapeParameters.CreateFilled(new ColoredGlyph(Color.Violet, Color.Black, 176), new ColoredGlyph(Color.White, Color.Black)));
+    Game.Instance.StartingConsole.DrawLine(new Point(60, 5), new Point(66, 20), '$', Color.AnsiBlue, Color.AnsiBlueBright, Mirror.None);
+    // X, Y, Color
+    Game.Instance.StartingConsole.SetForeground(15, 4, Color.DarkGreen);
+    // X, Y, Color
+    Game.Instance.StartingConsole.SetBackground(18, 4, Color.DarkCyan);
+    // X, Y, Glyph index or character
+    Game.Instance.StartingConsole.SetGlyph(4, 4, '@'); // use '@' or 1, they're the same index glyph
 }
